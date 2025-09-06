ba36689c241317bfa919e0a4e2d229847ae931e8
Peralta Rocio <peralta.rocio.2229@gmail.com>
2025-09-06 13:20:54 -0300

    ultimo ejercicio cinco

---
+++ ejercicioCinco.js
@@ -0,0 +1,16 @@
+//const prompt = require("prompt-sync")({ sigint: true });
+
+//*********************************************EJERCICIO CINCO
+
+// Enunciado:
+// Este ejercicio es ideal para familiarizarse con la manipulación de cadenas de
+// texto y la función prompt(). Escribe un programa que:
+// 1. Solicite al usuario que ingrese su nombre.
+// 2. Muestre un saludo personalizado usando el nombre ingresado.
+
+//SOLUCION 
+
+let nombreUsuario = prompt("Ingresa tu nombre");
+let apellidoUsuario = prompt ("Ingresa tu apellido");
+
+alert("Hola" + " " +nombreUsuario + " " + apellidoUsuario + " " + "¡bienvenida a Ada!"); 
