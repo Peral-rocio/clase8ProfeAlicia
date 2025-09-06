454462a80fb5227458d69dddaee6c69a9f4ea467
Peralta Rocio <peralta.rocio.2229@gmail.com>
2025-09-06 13:18:42 -0300

    ejercicio 4

---
+++ ejercicioCuatro.js
@@ -0,0 +1,21 @@
+//const prompt = require("prompt-sync")({ sigint: true });
+
+//**************************************************************EJERCICIO CUATRO
+
+// Ejercicio 4: Determinar si un número es par o impar
+// Enunciado:
+// En este ejercicio, aprenderás a utilizar operadores aritméticos y condicionales.
+// Debes escribir un programa que:
+// 1. Solicite al usuario que ingrese un número entero.
+// 2. Determine si el número es par o impar.
+// 3. Muestre un mensaje indicando si el número es par o impar.
+
+//SOLUCION 
+
+// let numeroEntero = prompt ("Ingrese un numero entero porfavor");
+
+// if (numeroEntero % 2 == 0) {
+//     alert("Este numero es PAR");
+// } else {
+//     alert ("Este numero es Impar");
+// }
